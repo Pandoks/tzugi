c39c9bf34e935fa9d469316fe482b76e457fefd5
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-02-15 12:04:42 -0500

    Middleware session auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,5 +9,25 @@ export const handle: Handle = async ({ event, resolve }) => {
 		return resolve(event);
 	}
 
+	const { session, user } = await lucia.validateSession(sessionId);
+	if (session && session.fresh) {
+		const sessionCookie = lucia.createSessionCookie(session.id);
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+	}
+
+	if (!session) {
+		const sessionCookie = lucia.createBlankSessionCookie();
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+	}
+
+	event.locals.user = user;
+	event.locals.session = session;
+
 	return resolve(event);
 };
