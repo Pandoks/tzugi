2d1404763220e6a947e91f6bdd6090133b53d90c
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-02-15 15:00:18 -0500

    Added logout functionality

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,28 @@
+import { lucia } from '$lib/server/auth';
+import { fail, type Actions, redirect } from '@sveltejs/kit';
+import type { PageServerLoad, PageServerLoadEvent } from './$types';
+
+export const load: PageServerLoad = async (event: PageServerLoadEvent) => {
+	if (!event.locals.user) {
+		return redirect(302, '/login');
+	}
+
+	return { username: event.locals.user.username };
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		if (!event.locals.session) {
+			return fail(401);
+		}
+
+		await lucia.invalidateSession(event.locals.session.id);
+		const sessionCookie = lucia.createBlankSessionCookie();
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+
+		return redirect(302, '/login');
+	}
+};
