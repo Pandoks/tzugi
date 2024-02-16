0c086e60d36384a16fd29d15df0d1fb86bd19d20
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-02-16 13:48:50 -0500

    Password reset token generator

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,6 +1,9 @@
 import { dev } from '$app/environment';
-import { luciaAdapter } from '$lib/db';
-import { Lucia } from 'lucia';
+import { db, luciaAdapter } from '$lib/db';
+import { passwordResets } from '$lib/db/schema';
+import { eq } from 'drizzle-orm';
+import { Lucia, generateId } from 'lucia';
+import { TimeSpan, createDate } from 'oslo';
 
 export const lucia = new Lucia(luciaAdapter, {
 	sessionCookie: {
@@ -29,3 +32,15 @@ interface DatabaseUserAttributes {
 	email: string;
 	email_verified: boolean;
 }
+
+export const createPasswordResetToken = async (userId: string): Promise<string> => {
+	// invalidate all existing tokens
+	await db.delete(passwordResets).where(eq(passwordResets.userId, userId));
+
+	const tokenId = generateId(40);
+	await db
+		.insert(passwordResets)
+		.values({ id: tokenId, userId: userId, expiresAt: createDate(new TimeSpan(1, 'h')) });
+
+	return tokenId;
+};
