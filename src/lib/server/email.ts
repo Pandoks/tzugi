c79400064a45ad94f1a9f546e10e40eeaa1dbb6f
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-02-18 16:46:45 -0500

    Added transaction to generateEmailVerificationCode

--- src/lib/server/email.ts
+++ src/lib/server/email.ts
@@ -10,16 +10,19 @@ export const generateEmailVerificationCode = async (
 	userId: string,
 	email: string
 ): Promise<string> => {
-	await db.delete(emailVerifications).where(eq(emailVerifications.userId, userId));
-	const code = generateRandomString(6, alphabet('0-9', 'A-Z'));
-	await db.insert(emailVerifications).values({
-		userId: userId,
-		email: email,
-		code: code,
-		expiresAt: createDate(new TimeSpan(5, 'm')) // 5 minutes
-	});
+	return await db.transaction(async (tx) => {
+		await tx.delete(emailVerifications).where(eq(emailVerifications.userId, userId));
 
-	return code;
+		const code = generateRandomString(6, alphabet('0-9', 'A-Z'));
+		await tx.insert(emailVerifications).values({
+			userId: userId,
+			email: email,
+			code: code,
+			expiresAt: createDate(new TimeSpan(5, 'm')) // 5 minutes
+		});
+
+		return code;
+	});
 };
 
 // TODO
