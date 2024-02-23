1de63f4e6794c76064b635de34c6d151d667d74c
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-02-23 13:16:04 -0500

    Added correct forms to email verification

--- src/routes/auth/verify-email/+page.svelte
+++ src/routes/auth/verify-email/+page.svelte
@@ -1,18 +1,38 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { verifyEmailFormSchema, type VerifyEmailFormSchema } from '$lib/validation';
+	import { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
 	import type { PageData } from './$types';
-	import VerifyEmail from './verify-email.svelte';
+	import { zodClient } from 'sveltekit-superforms/adapters';
 
 	export let data: PageData;
+	export let dataForm: SuperValidated<Infer<VerifyEmailFormSchema>> = data.form;
+	const form = superForm(dataForm, {
+		validators: zodClient(verifyEmailFormSchema)
+	});
+	const { form: formData, enhance } = form;
 </script>
 
 <div class="flex justify-center items-center h-screen">
 	<Card.Root class="w-[350px]">
-		<Card.Header>
-			<Card.Title>Email Verification</Card.Title>
-		</Card.Header>
-		<Card.Content>
-			<VerifyEmail data={data.form} />
-		</Card.Content>
+		<form method="POST" use:enhance>
+			<Card.Header>
+				<Card.Title>Email Verification</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<Form.Field {form} name="code">
+					<Form.Control let:attrs>
+						<Form.Label>Verification Code</Form.Label>
+						<Input {...attrs} bind:value={$formData.code} />
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+			</Card.Content>
+			<Card.Footer>
+				<Form.Button>Submit</Form.Button>
+			</Card.Footer>
+		</form>
 	</Card.Root>
 </div>
