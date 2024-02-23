e9c2fb47c1edfa27db7f9f3ca1925b0737add680
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-02-23 00:40:02 -0500

    Login form component

---
+++ src/routes/auth/login/login-form.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
+	import { type LoginFormSchema, loginFormSchema } from '$lib/validation';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	export let data: SuperValidated<Infer<LoginFormSchema>>;
+	const form = superForm(data, {
+		validators: zodClient(loginFormSchema)
+	});
+	const { form: formData, enhance } = form;
+</script>
+
+<form method="POST" use:enhance>
+	<Form.Field {form} name="username">
+		<Form.Control let:attrs>
+			<Form.Label>Username</Form.Label>
+			<Input {...attrs} bind:value={$formData.username} />
+		</Form.Control>
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Field {form} name="email">
+		<Form.Control let:attrs>
+			<Form.Label>Email</Form.Label>
+			<Input {...attrs} bind:value={$formData.email} />
+		</Form.Control>
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Field {form} name="password">
+		<Form.Control let:attrs>
+			<Form.Label>Password</Form.Label>
+			<Input {...attrs} bind:value={$formData.password} type="password" />
+		</Form.Control>
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Button>Submit</Form.Button>
+</form>
