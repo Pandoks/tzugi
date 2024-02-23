b262db61f7b68add3af46ab60fa1b87a91ff0183
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-02-22 19:32:03 -0500

    Changed db connection to take params

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'drizzle-kit';
 
-const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_DATABASE = process.env.DB_DATABASE;
 
-if (!DB_CONNECTION_STRING) {
-	throw new Error('DB_CONNECTION_STRING is required');
+console.log(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE);
+
+if (
+	!DB_USERNAME ||
+	DB_PASSWORD === undefined ||
+	DB_PASSWORD === null ||
+	!DB_HOST ||
+	!DB_PORT ||
+	!DB_DATABASE
+) {
+	throw new Error('env database variables are required');
 }
 
 export default defineConfig({
@@ -11,7 +24,11 @@ export default defineConfig({
 	driver: 'pg',
 	out: './migrations',
 	dbCredentials: {
-		connectionString: DB_CONNECTION_STRING
+		user: DB_USERNAME,
+		password: DB_PASSWORD,
+		host: DB_HOST,
+		port: parseInt(DB_PORT),
+		database: DB_DATABASE
 	},
 	verbose: true,
 	strict: true
