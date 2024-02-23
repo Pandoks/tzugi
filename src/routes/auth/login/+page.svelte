f2ac12f2e7faf21d17ca2b9196c562ed23fc3c8e
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-02-23 13:06:20 -0500

    Added correct forms to login

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,18 +1,52 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
 	import type { PageData } from './$types';
-	import LoginForm from './login-form.svelte';
+	import { loginFormSchema, type LoginFormSchema } from '$lib/validation';
+	import { zodClient } from 'sveltekit-superforms/adapters';
 
 	export let data: PageData;
+	const dataForm: SuperValidated<Infer<LoginFormSchema>> = data.form;
+	const form = superForm(dataForm, {
+		validators: zodClient(loginFormSchema)
+	});
+	const { form: formData, enhance } = form;
 </script>
 
 <div class="flex justify-center items-center h-screen">
 	<Card.Root class="w-[350px]">
-		<Card.Header>
-			<Card.Title>Login</Card.Title>
-		</Card.Header>
-		<Card.Content>
-			<LoginForm data={data.form} />
-		</Card.Content>
+		<form method="POST" use:enhance>
+			<Card.Header>
+				<Card.Title>Login</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<Form.Field {form} name="username">
+					<Form.Control let:attrs>
+						<Form.Label>Username</Form.Label>
+						<Input {...attrs} bind:value={$formData.username} />
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+				<Form.Field {form} name="email">
+					<Form.Control let:attrs>
+						<Form.Label>Email</Form.Label>
+						<Input {...attrs} bind:value={$formData.email} />
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+				<Form.Field {form} name="password">
+					<Form.Control let:attrs>
+						<Form.Label>Password</Form.Label>
+						<Input {...attrs} bind:value={$formData.password} type="password" />
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+			</Card.Content>
+			<Card.Footer>
+				<Form.Button>Submit</Form.Button>
+			</Card.Footer>
+		</form>
 	</Card.Root>
 </div>
