2b88f5c35e6b112151aaf20492bb1e0cd5f33f29
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-03-05 18:38:54 -0500

    Plaid transaction endpoint

---
+++ src/routes/api/plaid/transactions/+server.ts
@@ -0,0 +1,36 @@
+import { plaid } from '$lib/plaid';
+import { json } from '@sveltejs/kit';
+import type { RemovedTransaction, Transaction, TransactionsSyncRequest } from 'plaid';
+
+export const GET = async (event) => {
+	let databaseCursor = CURSOR;
+	let temporaryCursor = databaseCursor;
+
+	// new transaction updates since cursor
+	let added: Array<Transaction> = [];
+	let modified: Array<Transaction> = [];
+	let removed: Array<RemovedTransaction> = [];
+
+	let hasMore = true; // transactions are sent paginated
+	while (hasMore) {
+		const request: TransactionsSyncRequest = {
+			access_token: ACCESS_TOKEN,
+			cursor: temporaryCursor
+		};
+		const response = await plaid.transactionsSync(request);
+		const data = response.data;
+
+		added = added.concat(data.added);
+		modified = modified.concat(data.modified);
+		removed = removed.concat(data.removed);
+
+		hasMore = data.has_more;
+
+		temporaryCursor = data.next_cursor;
+	}
+	added = [...added].sort((first, second) => {
+		return Number(second.date > first.date) - Number(second.date < first.date);
+	});
+
+	return json({ added: added });
+};
