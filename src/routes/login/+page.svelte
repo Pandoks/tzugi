150e75f8fa67e00ebb69f94b1133289255766d77
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-03-05 11:20:03 -0500

    Shadcn to login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,45 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	export let form;
+	import * as Card from '$lib/components/ui/card';
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
+	import type { PageData } from './$types';
+	import { loginFormSchema, type LoginFormSchema } from '$lib/validation';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	export let data: PageData;
+	const dataForm: SuperValidated<Infer<LoginFormSchema>> = data.form;
+	const form = superForm(dataForm, {
+		validators: zodClient(loginFormSchema)
+	});
+	const { form: formData, enhance } = form;
 </script>
 
-<form method="post" use:enhance>
-	<input name="email" value={form?.email ?? ''} />
-	<input type="password" name="password" />
-	<button>Sign up</button>
-</form>
+<div class="flex justify-center items-center h-screen">
+	<Card.Root class="w-[350px]">
+		<form method="POST" use:enhance>
+			<Card.Header>
+				<Card.Title>Login</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<Form.Field {form} name="email">
+					<Form.Control let:attrs>
+						<Form.Label>Email</Form.Label>
+						<Input {...attrs} bind:value={$formData.email} />
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+				<Form.Field {form} name="password">
+					<Form.Control let:attrs>
+						<Form.Label>Password</Form.Label>
+						<Input {...attrs} bind:value={$formData.password} type="password" />
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+			</Card.Content>
+			<Card.Footer>
+				<Form.Button>Submit</Form.Button>
+			</Card.Footer>
+		</form>
+	</Card.Root>
+</div>
