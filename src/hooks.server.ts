7eabc3e88a6d9bad5e5d56eabdc3304ade56c5a3
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-03-05 10:59:52 -0500

    Added basic login

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,9 @@
-// src/hooks.server.ts
+import { type Handle, redirect, error } from '@sveltejs/kit';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
-import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle: Handle = async ({ event, resolve }) => {
+async function supabase({ event, resolve }) {
 	event.locals.supabase = createSupabaseServerClient({
 		supabaseUrl: PUBLIC_SUPABASE_URL,
 		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
@@ -27,4 +27,28 @@ export const handle: Handle = async ({ event, resolve }) => {
 			return name === 'content-range';
 		}
 	});
-};
+}
+
+async function authorization({ event, resolve }) {
+	// protect requests to all routes that start with /protected-routes
+	if (event.url.pathname.startsWith('/protected-routes') && event.request.method === 'GET') {
+		const session = await event.locals.getSession();
+		if (!session) {
+			// the user is not signed in
+			throw redirect(303, '/');
+		}
+	}
+
+	// protect POST requests to all routes that start with /protected-posts
+	if (event.url.pathname.startsWith('/protected-posts') && event.request.method === 'POST') {
+		const session = await event.locals.getSession();
+		if (!session) {
+			// the user is not signed in
+			throw error(303, '/');
+		}
+	}
+
+	return resolve(event);
+}
+
+export const handle: Handle = sequence(supabase, authorization);
