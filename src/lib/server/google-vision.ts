b2254e053c5fa4513815e8a3395dfd184ec000cd
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-03-22 21:50:00 -0400

    Added pagination shadcn

--- src/lib/server/google-vision.ts
+++ src/lib/server/google-vision.ts
@@ -1,28 +1,14 @@
 import vision from '@google-cloud/vision';
-import { type SupabaseClient } from '@supabase/supabase-js';
 import { blobToBuffer } from '../utils';
 
 const googleVisionClient = new vision.ImageAnnotatorClient();
 
-export const detectWordsFromImage = async ({
-	storageFilePath,
-	storageBucket,
-	cloudClient
-}: {
-	storageFilePath: string;
-	storageBucket: string;
-	cloudClient: SupabaseClient;
-}) => {
-	const { data, error } = await cloudClient.storage.from(storageBucket).download(storageFilePath);
-	if (error) {
-		return { data: null, error };
-	}
-	const image = await blobToBuffer(data);
-	const [result] = await googleVisionClient.textDetection(image);
+export const detectWordsFromImage = async (image: Blob) => {
+	const [result] = await googleVisionClient.textDetection(await blobToBuffer(image));
 	const detections = result.textAnnotations;
 	if (!detections) {
 		return { data: null, error: 'No text detected' };
 	}
 	const text = detections[0].description;
-	return { data: text, error };
+	return { data: text, error: null };
 };
